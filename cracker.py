85a184052aec0dd54fd35d518e7ba7973ef8a1a9
Bas van Driel <contact@basvandriel.nl>
2022-03-09 23:35:27 +0100

    make ests pass

--- cracker.py
+++ cracker.py
@@ -3,23 +3,42 @@ from typing import Final
 
 NOT_FOUND_MSG: Final = "PASSWORD NOT IN DATABASE"
 
-def crack_sha1_hash(hash: str) -> str:
+def crack_sha1_hash(hash: str, use_salts: bool = False) -> str:
     """
     Brute forces a SHA-1 hash and returns it
     """
+    passwords_file = open('passwords.txt', 'r')
+    salts_file = open('salts.txt', 'r')
 
-    file = open('passwords.txt', 'r')
+    # passwords = []
+    salts = salts_file.readlines()
+
 
     # Read and compare every password with the hash
-    for pw in file.readlines():
+    for pw in passwords_file.readlines():
         # Trim off the new line from the string
-        pw = pw.rstrip("\n").encode()
-    	
-        # Instantiate the hash and start comparing
-        hash_object: hashlib._Hash = hashlib.sha1(pw)
+        pw = pw.rstrip("\n")
+
+        #  ... each salt string from the file `salts.txt` should be appended AND prepended to each 
+        # password from `passwords.txt` before hashing and before comparing it to the hash passed into the function.
+        if use_salts:
+            for known_salt in salts:
+                known_salt = known_salt.rstrip("\n")
+
+                # Append and prepend the password
+                prependedhash: hashlib._Hash = hashlib.sha1(f"{known_salt}{pw}".encode())
+                appendedhash: hashlib._Hash = hashlib.sha1(f"{pw}{known_salt}".encode())
+
+                if prependedhash.hexdigest() == hash:
+                    return pw
+
+                if appendedhash.hexdigest() == hash:
+                    return pw
+
+        hash_object: hashlib._Hash = hashlib.sha1(pw.encode())
 
         if hash_object.hexdigest() != hash:
-            continue;
+            continue
 
         return pw.decode()
 
--- test_cracker.py
+++ test_cracker.py
@@ -23,3 +23,37 @@ def test_hash_4():
     expected = "01071988"
 
     assert actual == expected
+
+
+def test_hash_salted_1():
+    actual = crack_sha1_hash("53d8b3dc9d39f0184144674e310185e41a87ffd5", True)
+    expected = "superman"
+
+    assert actual == expected
+
+
+def test_hash_salted_2():
+    actual = crack_sha1_hash("da5a4e8cf89539e66097acd2f8af128acae2f8ae", True)
+    expected = "q1w2e3r4t5"
+
+    assert actual == expected
+
+
+def test_hash_salted_3():
+    actual = crack_sha1_hash("ea3f62d498e3b98557f9f9cd0d905028b3b019e1", use_salts=True)
+    expected = "bubbles1"
+
+    assert actual == expected
+
+
+def test_hash_salted_4():
+    actual = crack_sha1_hash("05bbf26a28148f531cf57872df546961d1ed0861", use_salts=True)
+    expected = "01071988"
+
+    assert actual == expected
+
+def test_not_in_database():
+    actual = crack_sha1_hash("03810a46a2c1a0eae58d9332f01c32bdcec9a01a")
+    expected = "PASSWORD NOT IN DATABASE"
+
+    assert actual == expected
