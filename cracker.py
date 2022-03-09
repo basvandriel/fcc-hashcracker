8684c11a05e66855680c9f0a9d1fc23dea0a2ea6
Bas van Driel <contact@basvandriel.nl>
2022-03-10 00:16:01 +0100

    cleanup cracker

--- cracker.py
+++ cracker.py
@@ -3,43 +3,37 @@ from typing import Final
 
 NOT_FOUND_MSG: Final = "PASSWORD NOT IN DATABASE"
 
+def sha1hash(txt: str) -> str:
+    txt = txt.encode()
+
+    hash = hashlib._Hash = hashlib.sha1(txt)
+    return hash.hexdigest()
+
+
 def crack_sha1_hash(hash: str, use_salts: bool = False) -> str:
     """
     Brute forces a SHA-1 hash and returns it
     """
-    passwords_file = open('passwords.txt', 'r')
-    salts_file = open('salts.txt', 'r')
-
-    # passwords = []
-    salts = salts_file.readlines()
-
+    passwords = open('passwords.txt', 'r').readlines()
 
     # Read and compare every password with the hash
-    for pw in passwords_file.readlines():
+    for pw in passwords:
         # Trim off the new line from the string
         pw = pw.rstrip("\n")
 
         #  ... each salt string from the file `salts.txt` should be appended AND prepended to each 
         # password from `passwords.txt` before hashing and before comparing it to the hash passed into the function.
         if use_salts:
-            for known_salt in salts:
-                known_salt = known_salt.rstrip("\n")
+            salts = open('salts.txt', 'r').readlines()
+            for salt in salts:
+                salt = salt.rstrip("\n")
 
-                # Append and prepend the password
-                prependedhash: hashlib._Hash = hashlib.sha1(f"{known_salt}{pw}".encode())
-                appendedhash: hashlib._Hash = hashlib.sha1(f"{pw}{known_salt}".encode())
-
-                if prependedhash.hexdigest() == hash:
+                # Map over the possibilities of salting
+                # and up if the hash is in the array of hashes
+                if hash in [sha1hash(x) for x in [salt + pw, pw + salt]]:
                     return pw
 
-                if appendedhash.hexdigest() == hash:
-                    return pw
-
-        hash_object: hashlib._Hash = hashlib.sha1(pw.encode())
-
-        if hash_object.hexdigest() != hash:
-            continue
-
-        return pw.decode()
+        if sha1hash(pw) == hash:
+            return pw
 
     return NOT_FOUND_MSG
